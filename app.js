dafb98a73519bcfb63bc57b2675c2893d9997b45
aliazeemabbas <aliazeem480@gmail.com>
2023-08-31 15:45:58 +0500

    api product example

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const { products } = require('./data');
 
-//setup static and middleware
-app.use(express.static('./public'))
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-// })
-
-app.all('*', (req, res) => {
-    res.status(404).send('Resource not found')
+app.get('/', (req, res)=>{
+    res.send('<h1> Home Page </h1> <a href="/api/products"> products </a>')
 })
 
-app.listen(5000, () => {
-    console.log(`Server is listening on port 5000`);
-});
+app.get('/api/products', (req, res)=>{
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product
+        return {id, name, image}
+    })
+    res.json(newProducts)
+})
+
+app.listen(5000, ()=>{
+    console.log('Server is listening on port 5000...');
+})
